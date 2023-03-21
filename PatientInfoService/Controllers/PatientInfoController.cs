ac1ece2d233e12bc79286a8b8782925804ee64b8
themonk-e <codenmad@gmail.com>
2023-03-21 15:27:00 +0530

    patient info controller added

---
+++ PatientInfoService/Controllers/PatientInfoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PatientInfoService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientInfoController : ControllerBase
+    {
+    }
+}
